8fdd3d5a67909fe11efde426dbe9f7aecca0486f
Valerochka1337 <valerochkayartsev88@gmail.com>
2024-01-28 23:56:15 +0300

    fixed time changing

--- app/scripts.js
+++ app/scripts.js
@@ -1,14 +1,8 @@
 function monthDiff(dateFrom, dateTo) {
-  // Ensure that dateFrom is before dateTo
-  if (dateFrom > dateTo) {
-    var temp = dateFrom;
-    dateFrom = dateTo;
-    dateTo = temp;
-  }
-
   var months = (dateTo.getFullYear() - dateFrom.getFullYear()) * 12;
   months -= dateFrom.getMonth();
-  months += dateTo.getMonth();
+  months += dateTo.getMonth() + 1;
+  console.log(dateTo.getFullYear(), dateTo.getMonth(), dateTo.getDate());
 
   return months <= 0 ? 0 : months;
 }
@@ -17,10 +11,10 @@ window.onload = function() {
   var startDate = new Date(2022, 9, 1);
   var now = new Date();
 
-  var fullMonths = monthDiff(now, startDate).toString();
+  var fullMonths = monthDiff(startDate, now).toString();
 
   document.getElementById("university-period").innerText = "(" + startDate.getMonth().toString() + ", " + startDate.getFullYear().toString() + "-...), " + fullMonths + " months";
   startDate = new Date(2023, 11, 1);
-  fullMonths = monthDiff(now, startDate).toString();
+  fullMonths = monthDiff(startDate, now).toString();
   document.getElementById("job1-period").innerText = "(" + startDate.getMonth().toString() + ", " + startDate.getFullYear().toString() + "-...), " + fullMonths + " months";
 };
